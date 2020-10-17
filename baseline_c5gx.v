cd5f14dd819903c7d44fb49abfe757790d15605a
Vano Mazashvili <36914777+vmazashvili@users.noreply.github.com>
2020-10-17 14:57:25 +0400

    using two always blocks

--- baseline_c5gx.v
+++ baseline_c5gx.v
@@ -1,12 +1,13 @@
+`timescale 1ns / 1ps
 module baseline_c5gx(
-        ///////// CLOCK /////////
-      input              CLOCK_125_p, ///LVDS
-      ///////// KEY ///////// 1.2 V ///////
-      input       [1:0]  KEY,
-      ///////// LEDR ///////// 2.5 V ///////
-      output      [1:0]  LEDR,
-      ///////// SW ///////// 1.2 V ///////
-      input       [1:0]  SW
+	///////// CLOCK /////////
+	input              CLOCK_125_p, ///LVDS
+	///////// KEY ///////// 1.2 V ///////
+	input       [1:0]  KEY,
+	///////// LEDR ///////// 2.5 V ///////
+	output      [1:0]  LEDR,
+	///////// SW ///////// 1.2 V ///////
+	input       [1:0]  SW
 );
 
 	wire clk;
@@ -15,59 +16,71 @@ module baseline_c5gx(
 	wire[1:0] switches;
 	wire buttonTrigger;
 	wire rstDeb;
-	reg [1:0] leds;
-	
-	assign LEDR[1:0]= leds;
+
 	assign clk = CLOCK_125_p;
 	assign switches = SW[1:0];
 	assign button = !KEY[1];
 	assign rst = !KEY[0];
-	
-	parameter green = 2'b00, blue = 2'b01, pink = 2'b10, yellow = 2'b11;
+reg [1:0] leds;
+
+	assign LEDR[1:0]= leds;
+
+	parameter green = 2'b00, blue = 2'b01,
+			 pink = 2'b10, yellow = 2'b11;
 
 
 	reg[1:0] next_state;
 	reg[1:0] current_state;
 	reg buttonPrev;
-
-	
-	assign buttonTrigger = !buttonPrev & button; 
+ 
+ 	always @(posedge clk) begin
+	buttonPrev <= button;
+end
+	assign buttonTrigger = !buttonPrev & button;
 
 
 	always @(posedge clk) begin
-		buttonPrev <= button;
-		
-		if(rst) begin
+	 
+		if(rst)
 			current_state <= green;
-		end else if(buttonTrigger ) begin
-			current_state <= next_state;
+		else if(buttonTrigger) begin
+			current_state <= green;
+					case(current_state)
+			green   : current_state <= blue;
+			blue    : current_state <= switches[0] == 1 ? yellow : pink;
+			pink    : current_state <= switches[0] == 1 ? blue : green;
+			yellow  : current_state <= switches[1] == 1 ? yellow : green;
+			default: current_state <=2'bxx;
+			endcase
 		end
 	end
 
-
-	always @(switches,current_state)begin
-			case(current_state)
-				green   : next_state <= blue;
-				blue    : next_state <= switches[0] == 1 ? yellow : pink;
-				pink    : next_state <= switches[0] == 1 ? blue : green;
-				yellow  : next_state <= switches[1] == 1 ? yellow : green;
-				default : begin
-					current_state <= green;
-					next_state <= blue;
-				end
-			endcase
-	end
-
-
-	always @(posedge clk) begin
-		if(rst) begin
+	always @(current_state, switches) begin
+if(rst) begin
 			leds[0] <= 0;
 			leds[1] <= 0;
 		end
 		else begin
-			leds[0] <= !current_state[1] && switches[0] || !current_state[0]; 
-			leds[1] <= !current_state[0] && switches[0] || !current_state[1] && !switches[0] || current_state[1] && current_state[0] && switches[1]; 
+			leds[0] <= !current_state[1] && switches[0] || !current_state[0];
+			leds[1] <= !current_state[0] && switches[0] || !current_state[1] 
+			&& !switches[0] || current_state[1] && current_state[0] && switches[1];
 		end
 	end
 
 endmodule 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
